4f336337681c18bc51cba93a4244e889cc62e97b
Nicolas <nico22utn@gmail.com>
2019-01-02 21:23:15 -0300

    modulos fixeados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { LayoutModule } from './modules/layout/layout.module';
+import { MaterialModule } from './modules/material/material.module';
+import { APP_ROUTES } from './app.routes';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule.forRoot(),
+    APP_ROUTES,
+    LayoutModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
